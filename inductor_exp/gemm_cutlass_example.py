6c0d89bf41ccfe219dd21c3aab4a7ed2d3b27959
Will Feng (DPER) <willfeng@fb.com>
2022-07-30 23:52:54 -0700

    update

--- inductor_exp/gemm_cutlass_example.py
+++ inductor_exp/gemm_cutlass_example.py
@@ -2,7 +2,7 @@ import torchinductor
 import torchinductor.config
 from torch.fx.experimental.proxy_tensor import make_fx
 
-from torchinductor.compile_fx import compile_fx_inner
+from torchinductor.compile_fx import compile_fx_inner, stream
 import torch
 import torch.fx as fx
 
@@ -20,6 +20,8 @@ tensor_B = torch.arange(4*5, device='cuda', requires_grad=True, dtype=torch.floa
 inps = [tensor_A, tensor_B]
 
 new_mod = compile_fx_inner(make_fx(f)(*inps), inps)
+stream.synchronize()
+torch.cuda.current_stream().wait_stream(stream)
 torch.cuda.synchronize()
 
 out = new_mod(*inps)[0]
