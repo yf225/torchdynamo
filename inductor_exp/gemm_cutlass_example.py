3790d67bba78d905534276c21aedac57735f141f
Will Feng (DPER) <willfeng@fb.com>
2022-07-30 23:50:00 -0700

    update

--- inductor_exp/gemm_cutlass_example.py
+++ inductor_exp/gemm_cutlass_example.py
@@ -20,6 +20,8 @@ tensor_B = torch.arange(4*5, device='cuda', requires_grad=True, dtype=torch.floa
 inps = [tensor_A, tensor_B]
 
 new_mod = compile_fx_inner(make_fx(f)(*inps), inps)
+torch.cuda.synchronize()
+
 out = new_mod(*inps)[0]
 
 torch.cuda.synchronize()
