e38f645f7e54d5cba8002db8f05276c5bf2fdf1f
Will Feng (DPER) <willfeng@fb.com>
2022-07-30 23:20:33 -0700

    update

--- inductor_exp/gemm_example.py
+++ inductor_exp/gemm_example.py
@@ -14,8 +14,8 @@ def f(a, b):
     c = torch.mm(a, b)
     return (c,)
 
-tensor_A = torch.randn(3, 4, device='cuda', requires_grad=True)
-tensor_B = torch.randn(4, 5, device='cuda', requires_grad=True)
+tensor_A = torch.arange(3*4, device='cuda', requires_grad=True).view(3, 4)
+tensor_B = torch.arange(4*5, device='cuda', requires_grad=True).view(4, 5)
 inps = [tensor_A, tensor_B]
 
 new_mod = compile_fx_inner(make_fx(f)(*inps), inps)
