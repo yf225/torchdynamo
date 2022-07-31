81f9635a0fa61ddb9fee168cc4b7d2ecbdef232a
Will Feng (DPER) <willfeng@fb.com>
2022-07-31 14:07:42 -0700

    update

---
+++ inductor_exp/gemm_mul_cutlass_example.py
@@ -0,0 +1,29 @@
+import torch
+import torchinductor
+from torch.fx.experimental.proxy_tensor import make_fx
+from torchinductor.compile_fx import compile_fx_inner
+
+
+# torchinductor.config.triton.use_mm = True
+torchinductor.config.triton.use_cutlass = True
+torchinductor.config.triton.cudagraphs = True
+torchinductor.config.debug = True
+
+def f(a, b, x):
+    c = torch.mm(a, b)
+    y = b * x
+    return (c, y)
+
+tensor_A = torch.arange(3*4, device='cuda', requires_grad=True, dtype=torch.float).view(3, 4)
+tensor_B = torch.arange(4*5, device='cuda', requires_grad=True, dtype=torch.float).view(4, 5)
+tensor_X = torch.arange(4*5, device='cuda', requires_grad=True, dtype=torch.float).view(4, 5)
+inps = [tensor_A, tensor_B, tensor_X]
+
+new_mod = compile_fx_inner(make_fx(f)(*inps), inps)
+
+out = new_mod(*inps)[0]
+
+print(out)
+
+pt_output = tensor_A @ tensor_B * tensor_X
+assert torch.allclose(out, pt_output)
